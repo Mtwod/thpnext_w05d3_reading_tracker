60840c3bca3086ffac97918a992d433d2f179ee8
Mtwod <maximewong@hotmail.com>
2021-02-03 18:43:39 +0100

    Add features favorites and readLaters

--- src/components/BookCard/index.jsx
+++ src/components/BookCard/index.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import './index.scss';
 
-const BookCard = (book) => {
-  const { isFav, read, title, thumbnailUrl, shortDescription } = book;
+const BookCard = (props) => {
+  const {
+    data,
+    onFavorite,
+    onReadLater,
+    isFav,
+    isReadLater,
+  } = props;
+
+  const {
+    title,
+    thumbnailUrl,
+    shortDescription,
+  } = data;
+
   return (
-    <div className="BookCard">
-      <div className="card" style={{ width: '18rem', height: '27rem' }}>
-        <img src={thumbnailUrl} className="card-img-top" alt="thumbnail of the book" />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{shortDescription}</p>
-          <div className="row">
-            <button className="btn btn-primary" type="button">{ isFav ? 'Remove from Favorite' : 'Add to Favorites'}</button>
-            <button className="btn btn-primary" type="button">{ read ? 'No more interested' : 'Read later'}</button>
+    <li className="BookCard card m-2">
+      <div className="card-body">
+        <div className="row">
+          <img src={thumbnailUrl || './noImage.png'} className="BookCard__thumbnail col" alt="thumbnail of the book" />
+          <div className="col">
+            <h5 className="BookCard__title card-title">{title}</h5>
+            <p className="BookCard__text card-text">{shortDescription || 'No description available.'}</p>
           </div>
         </div>
+        <div className="row">
+          <button className="BookCard__favorite btn btn-primary col p-1 mx-2" type="button" onClick={onFavorite}>{ isFav ? 'Remove from Favorites' : 'Add to Favorites'}</button>
+          <button className="BookCard__read btn btn-info col p-1 mx-2" type="button" onClick={onReadLater}>{ isReadLater ? 'No more interested' : 'Read later'}</button>
+        </div>
       </div>
-    </div>
+    </li>
   );
 };
 
--- src/components/Books/index.jsx
+++ src/components/Books/index.jsx
@@ -1,18 +1,31 @@
-import BookCard from 'components/BookCard';
 import React from 'react';
+import BookCard from 'components/BookCard';
 
 const Books = () => {
   const [books, setBooks] = React.useState();
+  const [favorites, setFavorites] = React.useState([]);
+  const [readLaters, setReadLaters] = React.useState([]);
+
   const fetchBooks = async () => {
-    try {
-      const response = await fetch('https://gist.githubusercontent.com/MathisDYKDan/76bc73ec77481ccb82677cc7c0d8b524/raw/a23c99027b9bfc1bfdb22e22ddcb4301a5f870ee/books.json');
-      const data = await response.json();
-      const booksList = data.books[0];
-      console.log(booksList);
-      setBooks(booksList);
-    } catch (error) {
-      console.error('An error has been catched:');
-      console.error(error);
+    const response = await fetch('https://gist.githubusercontent.com/MathisDYKDan/76bc73ec77481ccb82677cc7c0d8b524/raw/a23c99027b9bfc1bfdb22e22ddcb4301a5f870ee/books.json');
+    const data = await response.json();
+    const booksList = data.books[0];
+    setBooks(booksList);
+  };
+
+  const toggleFavorite = (bookId) => {
+    if (!favorites.includes(bookId)) {
+      setFavorites([...favorites, bookId]);
+    } else {
+      setFavorites([...favorites.filter((bookKey) => bookKey !== bookId)]);
+    }
+  };
+
+  const toggleReadLater = (bookId) => {
+    if (!readLaters.includes(bookId)) {
+      setReadLaters([...readLaters, bookId]);
+    } else {
+      setReadLaters([...readLaters.filter((bookKey) => bookKey !== bookId)]);
     }
   };
 
@@ -21,11 +34,24 @@ const Books = () => {
   }, []);
 
   return (
-    <ul className="Books">
+    <ul className="Books row">
       {books && (
-        {books.map((bookData) => (
-          <BookCard data={bookData} key={bookData.isbn} />
-        ))}
+        books.map((bookData, bookIndex) => {
+          const bookKey = `${bookData.isbn}-${bookIndex}`;
+          return (
+            <BookCard
+              data={bookData}
+              key={bookKey}
+              onFavorite={() => { toggleFavorite(bookKey); }}
+              isFav={favorites.includes(bookKey)}
+              onReadLater={() => { toggleReadLater(bookKey); }}
+              isReadLater={readLaters.includes(bookKey)}
+            />
+          );
+        })
+      )}
+      {!books && (
+        <p>Loading books...</p>
       )}
     </ul>
   );
